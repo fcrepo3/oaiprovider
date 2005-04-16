c1414ebacf23e729e5bca6fe824aeb99124d72d8
Chris Wilper <cwilper@duraspace.org>
2005-04-16 23:31:32 +0000

    started setting up testing

--- src/test/junit/fedora/services/oaiprovider/TestFedoraOAIDriver.java
+++ src/test/junit/fedora/services/oaiprovider/TestFedoraOAIDriver.java
@@ -1,21 +1,80 @@
 package fedora.services.oaiprovider;
 
-import junit.framework.TestCase;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+
+import junit.framework.*;
+
+import proai.*;
+import proai.driver.*;
+
+import fedora.services.oaiprovider.*;
 
 /**
  * @author Edwin Shin
  */
 public class TestFedoraOAIDriver extends TestCase {
 
+    private OAIDriver m_impl;
+
+	public TestFedoraOAIDriver(String name) { super (name); }
+
     public static void main(String[] args) {
         junit.textui.TestRunner.run(TestFedoraOAIDriver.class);
     }
 
-    public void testInit() throws Exception {
-        
+    public void setUp() {
+        m_impl = new FedoraOAIDriver();
+        m_impl.init(System.getProperties());
     }
-    
-    public void testGetLatestDate() throws Exception {
-        
+
+    //////////////////////////////////////////////////////////////////////////
+
+    public void testLatestDate() throws Exception {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        String latestDate = df.format(m_impl.getLatestDate());
+        System.out.println("Latest Date was " + latestDate);
+    }
+
+    public void atestIdentity() throws Exception {
+        StringWriter writer = new StringWriter();
+        m_impl.write(new PrintWriter(writer, true));
+        System.out.println("Result of writeIdentity:\n" + writer.toString());
+    }
+
+    public void atestFormats() throws Exception {
+        Iterator iter = m_impl.listMetadataFormats();
+        while (iter.hasNext()) {
+            MetadataFormat format = (MetadataFormat) iter.next();
+            String prefix = format.getPrefix();
+            String uri = format.getNamespaceURI();
+            String loc = format.getSchemaLocation();
+            System.out.println("Format prefix = " + prefix);
+            System.out.println("       uri    = " + uri);
+            System.out.println("       loc    = " + loc);
+        }
+    }
+
+    public void atestSets() throws Exception {
+        Iterator iter = m_impl.listSetInfo();
+        while (iter.hasNext()) {
+            SetInfo info = (SetInfo) iter.next();
+            String spec = info.getSetSpec();
+            System.out.println("Set spec = " + spec);
+            StringWriter writer = new StringWriter();
+            info.write(new PrintWriter(writer, true));
+            System.out.println("Result of write: \n" + writer.toString());
+        }
+    }
+
+    public void atestRecords() throws Exception {
+        Iterator iter;
+        Date fromDate = null;
+        Date untilDate = m_impl.getLatestDate();
+
+    }
+
+    public void tearDown() {
     }
 }
