68080f2c9622bb5fc757bf2b39f1a32f4fc2dc0c
Chris Wilper <cwilper@duraspace.org>
2005-04-18 16:14:02 +0000

    starting to return actual setInfo objects

--- src/java/fedora/services/oaiprovider/FedoraSetInfo.java
+++ src/java/fedora/services/oaiprovider/FedoraSetInfo.java
@@ -1,33 +1,43 @@
 package fedora.services.oaiprovider;
 
-import java.io.PrintWriter;
+import java.io.*;
 
-import proai.SetInfo;
-import proai.error.ServerException;
+import proai.*;
+import proai.error.*;
 
 /**
- * @author Edwin Shin
+ * SetInfo impl that includes setDescription elements for
+ * setDiss dissemination, if provided + available.
  */
 public class FedoraSetInfo implements SetInfo {
+
     private String m_setSpec;
-    
-    public FedoraSetInfo(String setSpec) {
+    private String m_setName;
+    private String m_setDiss;
+   
+    // if setDiss is null, descriptions don't exist, which is ok
+    public FedoraSetInfo(String setSpec, String setName, String setDiss) {
         m_setSpec = setSpec;
+        m_setName = setName;
+        m_setDiss = setDiss;
     }
     
-    /* (non-Javadoc)
-     * @see proai.SetInfo#getSetSpec()
-     */
     public String getSetSpec() {
         return m_setSpec;
     }
 
-    /* (non-Javadoc)
-     * @see proai.Writable#write(java.io.PrintWriter)
-     */
-    public void write(PrintWriter out) throws ServerException {
-        // TODO Auto-generated method stub
-        
+    public void write(PrintWriter out) throws RepositoryException {
+        out.println("<set>");
+        out.println("  <setSpec>" + m_setSpec + "</setSpec>");
+        out.println("  <setName>" + m_setSpec + "</setName>");
+        writeDescriptions(out);
+        out.println("</set>");
+    }
+
+    private void writeDescriptions(PrintWriter out) throws RepositoryException {
+        if (m_setDiss == null) return;
+        // TODO: actually retrieve the setDescription(s) from the dissemination, fail gracefully
+        out.println("  <setDescription>" + m_setDiss + "</setDescription>");
     }
 
 }
--- src/java/fedora/services/oaiprovider/FedoraSetInfoIterator.java
+++ src/java/fedora/services/oaiprovider/FedoraSetInfoIterator.java
@@ -1,6 +1,11 @@
 package fedora.services.oaiprovider;
 
-import org.jrdf.graph.*;
+import java.util.*;
+
+import org.apache.log4j.*;
+import org.jrdf.graph.Literal;
+import org.jrdf.graph.Node;
+import org.jrdf.graph.URIReference;
 import org.trippi.*;
 
 import proai.*;
@@ -9,10 +14,80 @@ import proai.error.*;
 
 public class FedoraSetInfoIterator implements RemoteIterator {
 
+    private static final Logger logger =
+            Logger.getLogger(FedoraOAIDriver.class.getName());
+
     private TupleIterator m_tuples;
 
-    public FedoraSetInfoIterator(TupleIterator tuples) {
+    private SetInfo m_next;
+
+    private String m_priorSetSpec;
+    private String m_priorSetName;
+
+    /**
+     * Initialize with tuples.
+     *
+     * The tuples should look like:
+     * <pre>
+     * "setSpec"    ,"setName"         ,"setDiss"
+     * prime        ,Prime             ,null
+     * prime        ,Prime             ,info:fedora/demo:SetPrime/SetInfo.xml
+     * abovetwo:even,Above Two and Even,null
+     * abovetwo:even,Above Two and Even,info:fedora/demo:SetAboveTwoEven/SetInfo.xml
+     * abovetwo:odd ,Above Two and Odd ,null
+     * abovetwo     ,Above Two         ,null
+     * abovetwo     ,Above Two         ,info:fedora/demo:SetAboveTwo/SetInfo.xml
+     * </pre>
+     */
+    public FedoraSetInfoIterator(TupleIterator tuples) throws RepositoryException {
         m_tuples = tuples;
+        m_next = getNext();
+    }
+
+    private SetInfo getNext() throws RepositoryException {
+        try {
+            if (m_tuples.hasNext()) {
+                String[] values = getValues(m_tuples.next());
+                String setSpec = values[0];
+                String setName = values[1];
+                String setDiss = values[2];
+                if (setDiss != null) {
+                    return new FedoraSetInfo(setSpec, setName, setDiss);
+                } else {
+                    return new FedoraSetInfo(setSpec, setName, setDiss);
+                    // if setDiss is null, check if it hasNext()
+                }
+            } else {
+                return null;
+            }
+        } catch (TrippiException e) {
+            throw new RepositoryException("Error getting next tuple", e);
+        }
+    }
+
+    private String[] getValues(Map valueMap) throws RepositoryException {
+        try {
+            String[] names = m_tuples.names();
+            String[] values = new String[names.length];
+            for (int i = 0; i < names.length; i++) {
+                values[i] = getString((Node) valueMap.get(names[i]));
+            }
+            return values;
+        } catch (Exception e) {
+            throw new RepositoryException("Error getting values from tuple", e);
+        }
+    }
+
+    private String getString(Node node) throws RepositoryException {
+       if (node == null) return null;
+       if (node instanceof Literal) {
+           return ((Literal) node).getLexicalForm();
+       } else if (node instanceof URIReference) {
+           return ((URIReference) node).getURI().toString();
+       } else {
+           throw new RepositoryException("Unhandled node type: " + node.getClass().getName());
+       }
+    }
 
 
 /*
@@ -43,17 +118,25 @@ public class FedoraSetInfoIterator implements RemoteIterator {
         }
         return null;
 */
-    }
 
     public boolean hasNext() throws RepositoryException {
-        return false;
+        return (m_next != null);
     }
 
     public Object next() throws RepositoryException {
-        return null;
+        try {
+            return m_next;
+        } finally {
+            if (m_next != null) m_next = getNext();
+        }
     }
 
     public void close() throws RepositoryException {
+        try {
+            m_tuples.close();
+        } catch (TrippiException e) {
+            throw new RepositoryException("Unable to close tuple iterator", e);
+        }
     }
 
     public void remove() throws UnsupportedOperationException {
